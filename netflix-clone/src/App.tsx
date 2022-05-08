b401bdcc74d8c95ba10fcfbafa4fe94b91f10560
GenkiHirano <genki86web@gmail.com>
2022-05-08 22:56:14 +0900

    nesflixCloneアプリ作成

---
+++ netflix-clone/src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Row } from "./components/Row";
+import { requests } from "./request";
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <Row
+          title="NETFLIX ORIGUINALS"
+          fetchUrl={requests.feachNetflixOriginals}
+          isLargeRow
+        />
+        <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+        <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
