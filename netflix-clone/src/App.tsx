fcec548ca60896eab9618aabbb10e4aa0d750728
GenkiHirano <genki86web@gmail.com>
2022-05-09 00:06:06 +0900

    Bannerコンポーネント作成

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Banner } from "./components/Banner";
 import { Row } from "./components/Row";
 import { requests } from "./request";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <div className="App">
+        <Banner />
         <Row
           title="NETFLIX ORIGUINALS"
           fetchUrl={requests.feachNetflixOriginals}
